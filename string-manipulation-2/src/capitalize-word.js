0b4fbb0da0d68d22f0378e86cdc1b219e0037ef8
Sarah You <yousarahh@gmail.com>
2023-03-27 16:48:58 +0000

    string-manipulation-2 WIP.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -3,6 +3,9 @@ function capitalizeWord(word) {
   let cap = '';
   for (let i = 0; i < word.length; i++) {
     cap = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    if (cap === 'Javascript') {
+      cap = word.charAt(0).toUpperCase() + word.slice(1, 4).toLowerCase() + word.charAt(4).toUpperCase() + word.slice(5).toLowerCase();
+    }
   }
   return cap;
 }
