5d79175d3e3b5bf7d7375de7aa8cee543e2d35a5
Sarah You <yousarahh@gmail.com>
2023-04-08 21:24:58 +0000

    completed javascript-local-storage.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,9 +1,13 @@
 /* exported todos */
 
 let todos = [];
+
 function beforeUnload(event) {
   const todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
 }
 
 window.addEventListener('beforeunload', beforeUnload);
+
+const previousTodosJSON = localStorage.getItem('javascript-local-storage');
+todos = JSON.parse(previousTodosJSON);
