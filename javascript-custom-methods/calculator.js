840b9305e4cb0e500a1e1efe198b4aa220568627
Sarah You <yousarahh@gmail.com>
2023-04-17 18:55:08 +0000

    completed javascript-custom-methods exercise.

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,33 @@
+/* exported calculator */
+const calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  },
+  getAverage: function (numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    const avg = sum / numbers.length;
+    return avg;
+  }
+};
