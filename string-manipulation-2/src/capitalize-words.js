69ee0065057ffd6b9d70a2eb35715781b62b1d4d
Sarah You <yousarahh@gmail.com>
2023-04-20 03:52:27 +0000

    WIP -- capitalizeWords

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,10 @@
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  // let words = '';
+  // for (let i = 0; i < string.length; i++) {
+  //   if (string.charAt(i) !== 1) {
+  //     words = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  //   }
+  // }
+  // return words;
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,12 @@
 /* exported ransomCase */
+function ransomCase(string) {
+  let word = '';
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      word += string.charAt(i).toLowerCase();
+    } else {
+      word += string.charAt(i).toUpperCase();
+    }
+  }
+  return word;
+}
