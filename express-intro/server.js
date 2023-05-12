687bcb03098bf0a179836eb6f9aeafe7585362a7
Sarah You <yousarahh@gmail.com>
2023-05-12 23:14:10 +0000

    updated notes

--- express-intro/server.js
+++ express-intro/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 // creates new Express application object;
 const app = express();
 
-// server is listening;
+// server is listening; PORT 8080 is what we use for express server;
 app.listen(8080, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 8080');
