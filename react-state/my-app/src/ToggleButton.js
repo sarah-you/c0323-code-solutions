cc88ba0cc5b0d5409a1938f94854750471801ef9
Sarah You <yousarahh@gmail.com>
2023-05-24 18:00:24 +0000

    moved the handleClick function

--- react-state/my-app/src/ToggleButton.js
+++ react-state/my-app/src/ToggleButton.js
@@ -2,18 +2,21 @@ import { useState } from 'react';
 
 export default function ToggleButton({ text, color }) {
   const [isClicked, setIsClicked] = useState(false);
+
+  function handleClick() {
+    alert(`${text} is Clicked!`);
+    console.log('value of isClicked before calling setter: ', isClicked);
+    setIsClicked(!isClicked);
+    console.log('value of isClicked after calling setter: ', isClicked);
+  }
+
   console.log('value returned by useState: ', isClicked);
   return (
     <button
+      onClick={handleClick}
       style={
         isClicked ? { backgroundColor: color } : { backgroundColor: 'white' }
-      }
-      onClick={() => {
-        alert(`${text} is Clicked!`);
-        console.log('value of isClicked before calling setter: ', isClicked);
-        setIsClicked(!isClicked);
-        console.log('value of isClicked after calling setter: ', isClicked);
-      }}>
+      }>
       {text}
     </button>
   );
