3641ca8140176f1e42e3d0fcd15f71657d281468
Sarah You <yousarahh@gmail.com>
2023-05-24 17:34:32 +0000

    added JSDoc notes

--- react-accordion-component/my-app/src/Accordion.js
+++ react-accordion-component/my-app/src/Accordion.js
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+/**
+ * Accordion manages state of active index of the topics using activeIndex
+ * the handleClick function updates the activeIndex by setting setActiveIndex to the clicked index or removing it if it is already matching index
+ * @param {}topics topics array is passed from parent (App()) to Accordion
+ * @returns RenderTopics component, passing to it topics, onClick, and currentIndex
+ */
 export default function Accordion({ topics }) {
   const [activeIndex, setActiveIndex] = useState();
   function handleClick(index) {
@@ -14,6 +20,15 @@ export default function Accordion({ topics }) {
   );
 }
 
+/**
+ * RenderTopics receives list of topics , currentIndex, and click handler function as props (from Accordion -- parent) and renders the topics to be displayed
+ * @param {}topics uses the map method to transform the topics array
+ * @returns jsx elements to be rendered
+ * @param {}onClick is passed from Accordion(parent) to handle when the user clicks on the title
+ * @returns the index of the clicked topic
+ * @param {}currentIndex to determine which content should be displayed by comparing the currentIndex with the index that was passed
+ * @returns the text associated with the topic
+ */
 function RenderTopics({ topics, onClick, currentIndex }) {
   const topicsList = topics.map((topics, index) => (
     <div key={index} className="topics-wrap">
