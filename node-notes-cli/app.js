688d99181d999b665a5a269b5e605f3d342fe444
Sarah You <yousarahh@gmail.com>
2023-05-29 23:55:51 +0000

    created a small command line CRUD app using Node.js

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,11 +1,49 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
+
+const rawData = await readFile('data.json', 'utf8');
+const data = JSON.parse(rawData);
+const action = process.argv[2];
+
+async function updateData(data) {
+  await writeFile('data.json', JSON.stringify(data, null, 2));
+}
 
 try {
-  const data = await readFile('data.json', 'utf8');
-  const parsedData = JSON.parse(data);
-  console.log(parsedData.notes);
-  const stringify = JSON.stringify(parsedData.notes, null, 2);
-  console.log(stringify);
+  switch (action) {
+    case 'read':
+      for (const prop in data.notes) {
+        console.log(`${prop}: ${data.notes[prop]}`);
+      }
+      break;
+    case 'create': {
+      data.notes[data.nextId] = process.argv[3];
+      data.nextId++;
+      updateData(data);
+      break;
+    }
+    case 'update': {
+      const id = process.argv[3];
+      if (data.notes[id]) {
+        data.notes[id] = process.argv[4];
+        updateData(data);
+      } else {
+        console.log('Note cannot be found');
+      }
+      break;
+    }
+    case 'delete': {
+      const id = process.argv[3];
+      if (data.notes[id]) {
+        delete data.notes[id];
+        updateData(data);
+      } else {
+        console.log('Note cannot be found');
+      }
+      break;
+    }
+    default:
+      console.log('Unknown command');
+  }
 } catch (err) {
   console.error(err.message);
 }
