d8a63e845c454671b319e020dad039ee1d7c4dcf
Sarah You <yousarahh@gmail.com>
2023-07-13 22:20:26 +0000

    capitalize-words

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,10 +1,13 @@
 /* exported capitalizeWords */
 function capitalizeWords(string) {
-  // let words = '';
-  // for (let i = 0; i < string.length; i++) {
-  //   if (string.charAt(i) !== 1) {
-  //     words = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-  //   }
-  // }
-  // return words;
+  let capWord = string[0].toUpperCase();
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      capWord += ' ' + string[i + 1].toUpperCase();
+      i++;
+    } else {
+      capWord += string[i].toLowerCase();
+    }
+  }
+  return capWord;
 }
