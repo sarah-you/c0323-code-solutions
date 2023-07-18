312c9a8165053af949d6132d6a0da6940221a3fe
Sarah You <yousarahh@gmail.com>
2023-07-18 02:47:39 +0000

    navbar done

---
+++ css-canvas-medical/my-app/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Main from './pages/Main';
+import Product from './pages/Product';
+import Implementation from './pages/Implementation';
+import AboutUs from './pages/AboutUs';
+import Blog from './pages/Blog';
+import ContactUs from './pages/ContactUs';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Main />} />
+          <Route path="product" element={<Product />} />
+          <Route path="implementation" element={<Implementation />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="contact" element={<ContactUs />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ css-canvas-medical/my-app/src/components/NavBar.js
@@ -0,0 +1,43 @@
+import './NavBar.css';
+import { Link, Outlet } from 'react-router-dom';
+
+
+export default function NavBar() {
+  return (
+    <div className="nb-container">
+      <div className="main-wrap">
+        <Link to="/" className="main">
+          <img src="/images/logo-blue.svg" alt="canvas blue logo" className='main-logo' />
+        </Link>
+      </div>
+      <div className="links-wrap">
+        <div className="product-wrap">
+          <Link to="/product" className="product">
+            Product
+          </Link>
+        </div>
+        <div className="implement-wrap">
+          <Link to="/implementation" className="implement">
+            Implementation
+          </Link>
+        </div>
+        <div className="about-wrap">
+          <Link to="/about" className="about">
+            About Us
+          </Link>
+        </div>
+        <div className="blog-wrap">
+          <Link to="/blog" className="blog">
+            Blog
+          </Link>
+        </div>
+        <div className="contact-wrap">
+          <Link to="/contact" className="contact">
+            Contact Us
+          </Link>
+        </div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
